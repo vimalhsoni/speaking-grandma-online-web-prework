ad83e937b49f0196cb1216850efad6b43b70e4c2
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-02 04:08:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 def speak_to_grandma(phrase)
   if phrase == 'What is UP?'
   return'HUH!? SPEAK UP SONNY!'
-elsif phrase == 'I LOVE YOU GRANDMA'
+elsif phrase == 'I LOVE YOU GRANDMA!'
   return 'I LOVE YOU TOO PUMPKIN'
 elsif phrase == phrase.upcase
   return 'NO, NOT SINCE 1938!'
-else 'HUH?! SPEAK UP SONNY!'
+else 'HUH?! SPEAK UP, SONNY!'
 end
 end
 # Whatever you say to grandma, she should respond with
