a6987d8cc20a23f2e6f2a0ea6e8211bef11cecca
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-02 04:09:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == 'What is UP?'
   return'HUH!? SPEAK UP SONNY!'
 elsif phrase == 'I LOVE YOU GRANDMA!'
-  return 'I LOVE YOU TOO PUMPKIN'
+  return 'I LOVE YOU TOO PUMPKIN!'
 elsif phrase == phrase.upcase
   return 'NO, NOT SINCE 1938!'
 else 'HUH?! SPEAK UP, SONNY!'
